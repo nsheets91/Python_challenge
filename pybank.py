59cf7d5b26a4f66629fdd045f804655cd663caef
nsheets91 <79558884+nsheets91@users.noreply.github.com>
2021-04-06 00:51:45 -0400

    Add files via upload

--- pybank.py
+++ pybank.py
@@ -9,9 +9,9 @@ net_total = 0
 profit_loss_change = 0
 avg_revenue_change = 0
 recent_month_gross = 0
-
+profit_loss_change = []
 # Open and read csv
-with open(financial_data) as csv_file:
+with open(financial_data, "r") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
 
     # Read the header row first (skip this part if there is no header)
@@ -22,45 +22,34 @@ with open(financial_data) as csv_file:
 
     # Read through each row of data after the header
     for row in csv_reader:
-        current_month = row[1] 
-        difference = (int(current_month) - (int(previous_month))
-
-        
- #Calculate the total revenue over the entire period
-
-     
-    net_total = int(row[1]) + 1   
-    previous_month = current_month
-    
+        current_month = row[0] 
+        total_months = total_months + 1
+        #Calculate the total revenue over the entire period
+        net_total = int(row[1]) + net_total    
+        previous_month = current_month
+        profit_loss_change.append(int(row[1]))
 
 # Calculate the changes in "Profit/Losses" & average revenue change as a float 
-profit_loss_change = float(row[1]) - previous_month
-previous_month = float(row[1])
-avg_revenue_change = sum(profit_loss_change) / sum(total_months)
+#profit_loss_change = float(row[1]) - previous_month
+#previous_month = float(row[1])
+avg_revenue_change = sum(profit_loss_change) / total_months
 
 #greatest increase/decrease in revenue 
 greatest_increase  = max(profit_loss_change)
 greatest_decrease = min(profit_loss_change)
 
 # Print the Results
-print("Financial Analysis")
-
-print("==================================================")
-
-print("total months" + str(total_months))
-print("net_total:" + "$" + str(sum(net_total)))
-print("Average change:" + "$" + str(avg_revenue_change))
-print("Greatest Increase in Profits:" +str(profit_loss_change[avg_revenue_change.index(max(avg_revenue_change))+1]) +  "$" + str(greatest_increase))
-print("Greatest decrease in Profits:" +str(profit_loss_change[avg_revenue_change.index(min(avg_revenue_change))+1]) + "$" + str(greatest_decrese))
-
-#Text File 
-file = open("export.txt")
-
-file.write("Financial Analysis")
-file.write("===============================================")
-file.write("Total: " + "$" + str(sum(net_total)))
-file.write("Average change:" + "$" + str(avg_revenue_change))
-file.write("Greatest Increase in Profits:" +str(profit_loss_change[avg_revenue_change.index(max(avg_revenue_change))+1]) + "" + "$" + str(greatest_increase))
-file.write("Greatest decrease in Profits:" +str(profit_loss_change[avg_revenue_change.index(min(avg_revenue_change))+1]) + "" + "$" + str(greatest_decrease))
-
-file.closed()
+analysis_output = (
+f"Financial Analysis\n"
+f"==================================================\n"
+f"Total Months: {str(total_months)}\n"
+f"Net Total: ${str(net_total)}\n"
+f"Average Change: ${str(round(avg_revenue_change, 2))}\n"
+f"Greatest increase in Profits: ${str(greatest_increase)}\n"
+f"Greatest decrease in Profits: ${str(greatest_decrease)}\n"
+)
+print(analysis_output)
+#str(profit_loss_change[avg_revenue_change.index(min(avg_revenue_change))+1])
+#str(profit_loss_change[avg_revenue_change.index(max(avg_revenue_change))+1])
+with open("analysis_output.txt", "w") as txt_file:
+    txt_file.write(analysis_output)
--- pypoll.py
+++ pypoll.py
@@ -1,49 +1,81 @@
 import os
 import csv 
 
-
-poll_data = os.path.join("Resources", "election_data.csv")
-
-#open csv
-with open('PyPoll/Resources/election_data.csv', newline= '') as csvfile:
-    csvreader = csv.reader(csvfile,delimiter=',')
-    next(csvreader, None)
-
-candidate = []
+candidates = []
 vote_count = {}
 percentage = {}
-
 total_votes = 0
 
 
+#open csv 
+with open('Resources/election_data.csv','r') as csvfile:
+    csvreader = csv.reader(csvfile,delimiter=',')
+    next(csvreader, None)
+    for row in csvreader[:1000]:
+        if row[2] not in candidates:
+            candidates.append(row[2])
+            #candidates.(row[2]):
+            vote_count[row[2]] = 1
 
+print(candidates)
 
+ # create vote counter
 
- # cycle through data and append candidate list with candidate
-                #0          #1       #2     #3
-   candiates = ["Correy", "Khan", "Li", "O'Tooley"]
-    For x in "candiate " in range (2)
-        print (x)   
-   string_candidate.append(1)
- # increases vote counter by 1
-vote_count = int(row[0]) + 1 
+if row[2] in candidate and row[2] not in "Candidate":
+    vote_count[row[2]] = vote_count[row[2]] + 1
+        #else candidate.(row[2]):
+        #    vote_count[row[2]] = 1
+
   # The total number of votes cast
+  total_votes = int(row[0]) + vote_count    
 
-  # A complete list of candidates who received votes
 
   # The percentage of votes each candidate won
-   str(round((value/total_votes)*100,3)) + "%" ("+str(value)+ ")"
+for percentages
+    str(round(total_votes)*100)/(total_votes)
 
   # The total number of votes each candidate won
+for "Kahn": len(row[2]("kahn")
+    total_votes = int(row[0]) + 1
+     else candidate.(row[2]):
+            vote_count[row[2]] = 1
+
+for "Correy": len(row[2]("Correy")
+     total_votes = int(row[0]) + 1
+     else candidate.(row[2]):
+            vote_count[row[2]] = 1
+
+for "Li": len(row[2]("Li")
+     total_votes = int(row[0]) + 1
+     else candidate.(row[2]):
+            vote_count[row[2]] = 1
+
+for "O'tooley":len(row[2]("O'tooley")
+    total_votes = int(row[0]) + 1
+     else candidate.(row[2]):
+            vote_count[row[2]] = 1
 
   # The winner of the election based on popular vote.
 
-  # As an example, your analysis should look similar to the one below:
+for winner = winner(str)(max(vote_count))
 
 
-           
 
-
+        
+eletion_output = (
+f"election Results\n"
+f"==================================================\n"
+f"Total votes: {str(candidates)}\n"
+f"==================================================\n"
+f"Precentage: {percentage}%\n"
+f"==================================================\n"
+f"Winner: {winner}\n"
+f"Greatest increase in Profits: ${str(greatest_increase)}\n"
+f"Greatest decrease in Profits: ${str(greatest_decrease)}\n"
+)
+print(analysis_output)
+with open("election_output.txt", "w") as txt_file:
+    txt_file.write(election_output)
 
 
 
